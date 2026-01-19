4e7e496731efe18aada2bfd39453324911bcef59
Sreejit Sadhukhan <sreejit.s501@gmail.com>
2026-01-20 01:36:52 +0530

    minor error handling

--- BackEnd/src/lib/db.js
+++ BackEnd/src/lib/db.js
@@ -4,6 +4,9 @@ import { ENV } from "./env.js";
 
 export const connectDB=async()=>{
      try{
+        if(!ENV.DATABASE_URL){
+            throw new Error("DATABASE_URL is not defined in environment variables");
+        }
        const conn= await mongoose.connect(ENV.DATABASE_URL);
          console.log("MongoDB connected:",conn.connection.host);
      }
