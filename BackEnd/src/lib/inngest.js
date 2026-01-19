05ca1e3cf42d6601a72696b3cba28a9ec4c98d40
Sreejit Sadhukhan <sreejit.s501@gmail.com>
2026-01-20 02:30:35 +0530

    small fix added

--- BackEnd/src/lib/inngest.js
+++ BackEnd/src/lib/inngest.js
@@ -3,7 +3,7 @@ import { connectDB } from "./db.js"
 import User from "../models/User.js"
 
 
-export const inngest=Inngest({
+export const inngest=new Inngest({
     id:"interview-platform-1",
 });
 
