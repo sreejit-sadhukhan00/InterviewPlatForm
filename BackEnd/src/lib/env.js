fb14e06c775c36f8a33e4175d35fcbbd6b2ea700
Sreejit Sadhukhan <sreejit.s501@gmail.com>
2026-01-20 02:23:35 +0530

    Auth And Db Sync With Inngest

--- BackEnd/src/lib/env.js
+++ BackEnd/src/lib/env.js
@@ -5,4 +5,12 @@ export const ENV={
     PORT:process.env.PORT,
     DATABASE_URL:process.env.DATABASE_URL,
     NODE_ENV:process.env.NODE_ENV,
+    CLIENT_URL:process.env.CLIENT_URL,
+    INNGEST_EVENT_KEY:process.env.INNGEST_EVENT_KEY,
+    INGEST_SIGNING_KEY:process.env.INGEST_SIGNING_KEY,
+    STREAM_API_KEY:process.env.STREAM_API_KEY,
+    STRAM_API_SECRET:process.env.STRAM_API_SECRET,
+    
+CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
+CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
 }
--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,20 +2,33 @@ import express from "express";
 import dotenv from "dotenv";
 import { ENV } from "./lib/env.js";
 import path from "path";
-import { connect } from "http2";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import {serve} from "inngest/express"
+import { inngest } from "./lib/inngest.js";
 dotenv.config();
 
 
 const app = express();
 
 const __dirname=path.resolve();
+// middleware 
 app.use(express.json());
+app.use(cors(
+    {
+       origin:ENV.CLIENT_URL  ,
+       credenetials:true
+    }
+));
 
 app.get("/health",(req,res)=>{
      res.status(200).send("Server is healthy");
 })
  
+app.use("/api/inngest",serve({
+    client:inngest,functions
+}));
+
 // make our app development ready 
  if(ENV.NODE_ENV==="production"){
      app.use(express.static(path.join(__dirname,"../Frontend/dist")));
