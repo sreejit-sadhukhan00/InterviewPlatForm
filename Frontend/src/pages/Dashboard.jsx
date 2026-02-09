18a51e1752712ae3fbbbc9aca2814d2a51b4d6e9
Sreejit Sadhukhan <sreejit.s501@gmail.com>
2026-02-09 10:25:14 +0530

    probles-page added

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div> 
+      <NavBar/>
+    </div>
   )
 }
 
--- Frontend/src/pages/ProblemsPage.jsx
+++ Frontend/src/pages/ProblemsPage.jsx
@@ -1,8 +1,102 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import { PROBLEMS } from '../../data/problems'
+import { Link } from 'react-router';
+import { ChevronRightIcon, Code2Icon } from 'lucide-react';
+import { getDifficultyBadgeClass } from '../lib/utils';
 
 const ProblemsPage = () => {
+   const problems= Object.values(PROBLEMS);
+    const easyproblems=problems.filter(problem=>problem.difficulty==='Easy').length;
+    const mediumproblems=problems.filter(problem=>problem.difficulty==='Medium').length;
+    const hardproblems=problems.filter(problem=>problem.difficulty==='Hard').length;
   return (
-    <div>ProblemsPage</div>
+    <div className='min-h-screen bg-base-200'>
+        <NavBar/>
+
+         <div className='max-w-6xl mx-auto px-4 py-12'>
+              {/* header */}
+               <div className='mb-8'>
+                 <h1 className='text-4xl font-bold mb-2'>Practice Problems</h1>
+                    <p className='text-base-content/70'>
+                      Solve coding challenges to sharpen your skills and prepare for interviews.
+                    </p>
+               </div>
+
+               {/* problems list  */}
+                 <div className='space-y-4 flex flex-col flex-wrap gap-4'>
+                      {problems.map(problem=>(
+                        <Link key={problem.id}
+                         to={`/problem/${problem.id}`}
+                         className='card bg-base-100 hover:scale-[1.01] transition-transform'
+                        >
+                           <div
+                            className='card-body'
+                           >
+                              <div className='flex items-center justify-between gap-4'>
+                                {/*  Left side*/}
+                                  <div className='flex-1'>
+                                     <div className='flex items-center gap-3 mb-2'>
+                                          <div className='size-12 rounded-lg bg-primary/10 flex items-center justify-center ' >
+                                               <Code2Icon className='size-5 text-primary'/>
+                                          </div>
+
+                                          <div className='flex-1'>
+                                               <div className='flex items-center gap-2 mb-1'>
+                                                <h2 className='text-lg font-bold'>
+                                                  {problem.title} 
+                                                </h2>
+                                                   <span className={`badge ${getDifficultyBadgeClass(problem.difficulty)}`}>
+                                                      {problem.difficulty}
+                                                      </span> 
+                                               </div>
+                                               <p className='text-sm text-base-content/70 '>
+                                                  {problem.category}
+                                               </p>
+                                           </div>
+                                     </div>
+                                        <p className=' text-base-content/70 ml-6 mb-3'>
+                                          {problem.description.text} 
+                                        </p>
+                                  </div>
+                                 {/* Right side */}
+                                   <div className='flex items-center gap-2 text-primary text-lg'>
+                                      solve
+                                      <ChevronRightIcon className='size-5'/>
+                                   </div>
+                              </div>
+                           </div>
+                         </Link>
+                      ))}
+                 </div>
+
+                {/* stats footer */}
+                 <div className='mt-12 card bg-base-100 shadow-lg'>
+                   <div className='card-body'>
+                        <div className='stats stats-vertical lg:stats-horizontal'>
+                             <div className='stat'> 
+                                <div className='stat-title'>Total Problems</div>
+                                <div className='stat-value text-primary'>{problems.length}</div>
+                             </div>
+                             <div className='stat'> 
+                                <div className='stat-title'>Easy Problems</div>
+                                <div className='stat-value text-primary'>{easyproblems}</div>
+                             </div>
+                             <div className='stat'> 
+                                <div className='stat-title'>Medium Problems</div>
+                                <div className='stat-value text-primary'>{mediumproblems}</div>
+                             </div>
+                             <div className='stat'> 
+                                <div className='stat-title'>Hard Problems</div>
+                                <div className='stat-value text-primary'>{hardproblems}</div>
+                             </div>
+                             
+                        </div>
+                   </div>
+                     
+                 </div>
+         </div>
+    </div>
   )
 }
 
